dc8fa56c20ab01ebde83941a0054508caa1f352b
Michal Šutek <sutek.spsit@gmail.com>
2024-12-18 12:34:20 +0100

    create templates

--- skola/urls.py
+++ skola/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('studenti/', views.list_students, name="list-students"),
+    path('ucitelia/', views.list_teachers, name="list-teachers"),
+    path('triedy/', views.list_triedy, name="list-class"),
 ]
--- skola/views.py
+++ skola/views.py
@@ -3,4 +3,18 @@ from . models import * # naimportovanie všetkých modelov z models.py v tomto p
 
 def index(request):
     studenti = Student.objects.all()
-    return HttpResponse(studenti)
+    ucitelia = Ucitel.objects.all()
+    triedy = Trieda.objects.all()
+    return render(request, "skola/index.html", {"studenti":studenti, "ucitelia":ucitelia, "triedy":triedy})
+
+def list_students(request):
+    studenti = Student.objects.all()
+    return render(request, "skola/index.html", {"studenti":studenti})    
+
+def list_teachers(request):
+    ucitelia = Ucitel.objects.all()
+    return render(request, "skola/index.html", {"ucitelia":ucitelia})
+
+def list_triedy(request):
+    triedy = Trieda.objects.all()
+    return render(request, "skola/index.html", {"triedy":triedy})    
